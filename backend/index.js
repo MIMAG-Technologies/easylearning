9133261fb7ec8bac593349edf7e422ee3a66fb12
aniketraut16 <rautan_1@rknec.edu>
2024-05-23 15:07:24 +0530

    Set Up Backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connectDB = require("./config/connection");
+require("dotenv").config();
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
